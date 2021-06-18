faa506fb62f845168b9874a720c0b62808245058
Tata <tspan2017@gmail.com>
2021-06-19 01:53:57 +0200

    Add mptt category model and adjust admin

--- backend/shop_time/categories/admin.py
+++ backend/shop_time/categories/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Category
+from mptt.admin import MPTTModelAdmin
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id', 'slug')
-    list_display_links = ['id', 'name']
-    list_per_page = 25
-    
+
+
+class CategoryAdmin(MPTTModelAdmin):
+    mptt_indent_field = "name"
+    mptt_level_indent = 20
+    list_display = ('name',) 
+   
+
+
+admin.site.register(Category, CategoryAdmin)
+ 
--- backend/shop_time/categories/models.py
+++ backend/shop_time/categories/models.py
@@ -1,16 +1,36 @@
 from django.db import models
 from autoslug import AutoSlugField
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=200,unique=True)  
     slug = AutoSlugField(populate_from='name',unique=True)
-    parent = models.ForeignKey('self',related_name='children',on_delete=models.CASCADE,blank=True,null=True)   
-
+    parent = TreeForeignKey('self',
+                            on_delete=models.CASCADE,
+                            null=True,
+                            blank=True,
+                            db_index=True,
+                            related_name='children'
+                            )
+    
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name = 'Category'
+        
+
+    class MPTTMeta:
+        level_attr = 'mptt_level'
+        order_insertion_by = ['name']
+
+    class Meta:
         verbose_name_plural = 'Categories'
+        unique_together = (('parent', 'slug',))
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
